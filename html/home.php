2335cf1ba534eccde0c27b540457ba0a6aeca1ca
rouaix <daniel@rouaix.com>
2018-09-25 14:40:50 +0200

    Maj

---
+++ html/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include_once "inc/head.php"; ?>
+<body class="">
+<header class="">    
+    <div class="logo"></div>
+    <nav class="">
+        <ul class="">
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+        </ul>
+    </nav>
+</header>
+<article class=""><!-- Full width -->
+    <section><!-- Background animé à définir -->
+        <h1 class=""><span class=""></span></h1>
+        <p class=""></p>
+    </section>
+    <section><!-- Bas de page visible page home -->
+        <button class=""></button>
+        <button class=""></button>
+        <button class=""></button>
+        <button class=""></button>
+    </section>
+</article>
+
+
+<aside class=""></aside>
+
+
+
+
+</body>
+<?php include_once "inc/footer.php"; ?>
+</html>
+
+
---
+++ html/inc/head.php
@@ -0,0 +1,38 @@
+<!-- Style.css -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700" rel="stylesheet">
+
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<title>$variable_</title>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="author" content="$variable_">
+<meta name="copyright" content="$variable_">
+<meta name="keywords" content="$variables_">
+
+<!-- Début Section OG 
+Définition du protocole Open Graph
+Le définition officielle du protocole Open Graph indique que c'est un protocole qui permet à une page web de devenir un objet "riche" dans un réseau social.
+Plus concrètement, le protocole Open Graph est un ensemble de balises qui permet à un webmaster de donner aux principaux réseaux sociaux (Facebook, Google +, Twitter, Linked in...) des informations précises sur ses pages. Ces informations permettront aux réseaux sociaux de mieux afficher un lien sur une ces pages qui aurait été placé par leur utilisateurs.
+Le protocole Open Graph a été crée à l'origine par Facebook et est désormais géré par l'Open Web Fondation.
+-->
+
+<meta property="og:url" content="$variable_" />
+<meta property="og:site_name" content="$variable_" />
+<meta property="og:locale" content="fr_FR" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="$variable_" />
+<meta property="og:description" content="$variable_" />
+<meta property="og:image" content="$variable_" />
+<!-- Fin Section OG -->
+
+<!-- Début liens CSS -->
+<link rel="stylesheet" type="text/css" href="views/css/style.css">q
+<!-- Fin liens CSS -->
