d8d171df2292f8fdac9ece7024991735699c528c
symfony-lh <symfony@applh.com>
2018-09-26 12:20:15 +0000

    ajout code autour.php

--- autour.php
+++ autour.php
@@ -1,2 +1,41 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php include_once "html/inc/head.php"; ?>
+<body class="accueil">
+<header class="">    
+    <img src="media/c4m.png" alt="" class="logo" />
+    <nav class=""><!-- Navigation -->
+        <ul class="hidden">
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+            <li class=""></li>
+        </ul>
+    </nav>
+</header>
+<article class=""><!-- Full width -->
+    
+    <section id="intro" class="text-center"><!-- Background animé à définir -->
+        <h1 class="">Découvrez en direct les merveilles de Marseille. <span class="hashtag">#Panier</span></h1>
+        <p class="">Une ville contemporaine qui abrite des petits villages de pêcheurs et des quartiers alternatifs.</p>
+    </section>
+
+    <section id="bandeau"><!-- Bas de page visible page home avec fond transparent-->
+        <div class="">
+        <a href="" title=""><button class="btn-medium">Choisissez une ville</button></a>
+        <a href="" title=""><button class="btn-medium">Choisissez une activité</button></a>
+        <a href="bons_plans.php" title=""><button class="btn-medium">Découvrir les bons plans</button></a>
+        <a href="autour.php" title=""><button class="btn-medium">Autour de moi</button></a>
+        </div>
+    </section>
+
+    <section>
+        <!-- Bas de page contenu à définir -->
+    </section>
+</article>
+<aside class=""></aside><!-- Block flottant à droite -->
+</body>
+<?php include_once "html/inc/footer.php"; ?>
+</html>
