8f9b81f396a3ed1bee64cd96588c5d4782ac9655
symfony-lh <symfony@applh.com>
2018-09-26 13:21:24 +0000

    ajout de chargement de leafletjs css+js dans head

--- autour.php
+++ autour.php
@@ -16,20 +16,38 @@
     </nav>
 </header>
 <article class=""><!-- Full width -->
-    
-    <section id="intro" class="text-center"><!-- Background animé à définir -->
-        <h1 class="">Découvrez en direct les merveilles de Marseille. <span class="hashtag">#Panier</span></h1>
-        <p class="">Une ville contemporaine qui abrite des petits villages de pêcheurs et des quartiers alternatifs.</p>
-    </section>
 
-    <section id="bandeau"><!-- Bas de page visible page home avec fond transparent-->
-        <div class="">
-        <a href="" title=""><button class="btn-medium">Choisissez une ville</button></a>
-        <a href="" title=""><button class="btn-medium">Choisissez une activité</button></a>
-        <a href="bons_plans.php" title=""><button class="btn-medium">Découvrir les bons plans</button></a>
-        <a href="autour.php" title=""><button class="btn-medium">Autour de moi</button></a>
+    <section class="container">
+        <div class="row">
+            <div class="col-12">
+<form>
+  <div class="form-group row">
+      <div class="col-sm-4">
+        <select class="form-control" id="ville" placeholder="ville">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="col-sm-4">
+        <select class="form-control" id="activite" placeholder="activité">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="col-sm-4">
+        <button type="submit" class="btn btn-primary mb-2">Valider</button>
+      </div>
+  </div>
+</form>                
+            </div>
         </div>
-    </section>
+    </section>    
 
     <section>
         <!-- Bas de page contenu à définir -->
