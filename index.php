d19e17408624e2ae627c3c450d6c65f8af520a0d
symfony-lh <symfony@applh.com>
2018-09-26 20:59:24 +0000

    merge index.php

--- index.php
+++ index.php
@@ -1,52 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 <?php include_once "html/inc/head.php"; ?>
-<<<<<<< HEAD
-<body class="accueil">
-<header class="">
-    <img src="media/c4m.png" alt="" class="logo" />
-    <nav class=""><!-- Navigation -->
-        <ul class="hidden">
-            <li class=""></li>
-            <li class=""></li>
-            <li class=""></li>
-            <li class=""></li>
-            <li class=""></li>
-            <li class=""></li>
-        </ul>
-    </nav>
-</header>
-
-<article class=""><!-- Full width -->
-
-    <section id="intro" class="text-center"><!-- Background animé à définir -->
-        <h1 class="">Découvrez en direct les merveilles de Marseille. <span class="hashtag">#Panier</span></h1>
-        <p class="">Une ville contemporaine qui abrite des petits villages de pêcheurs et des quartiers alternatifs.</p>
-    </section>
-
-    <section id="bandeau"><!-- Bas de page visible page home avec fond transparent-->
-        <span class="dropdown">
-            <button class="dropdown btn-medium" data-toggle="dropdown">Choisissez une ville
-                <div class="dropdown-menu">
-                    <div class="dropdown-item">Marseille</div>
-                    <div class="dropdown-item">Plan de Cuques</div>
-                    <div class="dropdown-item">Allauch</div>
-                    <div class="dropdown-item">Aix en Provence</div>
-                </div>
-            </button>
-</span>
-        <a href="" title=""><button class="btn-medium bouton">Choisissez une activité</button></a>
-        <a href="bons_plans.php" title=""><button class="btn-medium bouton">Découvrir les bons plans</button></a>
-        <a href="autour.php" title=""><button class="btn-medium bouton">Autour de moi</button></a>
-    </section>
-=======
-
 <body class="accueil">
     <header class="">
         <img src="media/c4m.png" alt="" class="logo" />
         <nav class="">
             <!-- Navigation -->
             <ul class="hidden">
+                <li class=""></li>
+                <li class=""></li>
+                <li class=""></li>
                 <li class=""></li>
                 <li class=""></li>
                 <li class=""></li>
@@ -63,7 +26,6 @@
                 <p class="">Une ville contemporaine qui abrite des petits villages de pêcheurs et des quartiers
                     alternatifs.</p>
             </section>
->>>>>>> ca3aafaab2ea3433e4ea55c53035105290401a65
 
             <section id="bandeau">
                 <!-- Bas de page visible page home avec fond transparent-->
@@ -103,9 +65,4 @@
 <?php include_once "html/inc/footer.php"; ?>
 
 </body>
-<<<<<<< HEAD
 </html>
-=======
-
-</html>
->>>>>>> ca3aafaab2ea3433e4ea55c53035105290401a65
