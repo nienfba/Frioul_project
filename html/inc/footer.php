e956252caa12fa43c43d7a440fe19f60d0f23381
symfony-lh <symfony@applh.com>
2018-09-26 12:36:33 +0000

    ajout texte dans footer

--- html/inc/footer.php
+++ html/inc/footer.php
@@ -6,6 +6,11 @@
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mx-auto">
             Footer
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mx-auto"></div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 mx-auto">
+            <h4>Code4Marseille</h4>
+            <p>167 rue Paradis</p>
+            <p>13006 Marseille</p>
+            <p>email:<a href="mailto:contact@code4marseille.fr">contact@code4marseille.fr</a></p>
+        </div>
     </div>
 </footer>
