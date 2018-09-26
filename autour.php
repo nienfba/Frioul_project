e102fe1f0111223e805c9c32e3e5096b50f60b4c
symfony-lh <symfony@applh.com>
2018-09-26 13:39:32 +0000

    ajout carte leafletjs

--- autour.php
+++ autour.php
@@ -3,7 +3,7 @@
 <?php include_once "html/inc/head.php"; ?>
 <body class="autour">
 <header class="">    
-    <img src="media/c4m.png" alt="" class="logo" />
+    <a href="index.php"><img src="media/c4m.png" alt="" class="logo" /></a>
     <nav class=""><!-- Navigation -->
         <ul class="hidden">
             <li class=""></li>
@@ -24,20 +24,20 @@
   <div class="form-group row">
       <div class="col-sm-4">
         <select class="form-control" id="ville" placeholder="ville">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option value="">choisissez une ville</option>
+          <option>marseille</option>
+          <option>aix</option>
+          <option>aubagne</option>
+          <option>cassis</option>
         </select>
       </div>
       <div class="col-sm-4">
         <select class="form-control" id="activite" placeholder="activité">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+          <option value="">choisissez une activité</option>
+          <option>Espace Naturel</option>
+          <option>Mer</option>
+          <option>Montagne</option>
+          <option>Urbain</option>
         </select>
       </div>
       <div class="col-sm-4">
@@ -49,6 +49,28 @@
         </div>
     </section>    
 
+    <section class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                LISTE DES BONS PLANS
+            </div>
+            <div class="col-sm-6">
+                <div id="map">
+                </div>
+                <script>
+var mymap = L.map('map').setView([43.3, 5.4], 10);
+this.tileLayer = L.tileLayer(
+                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png', {
+                    maxZoom: 18,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                }
+            );                
+            this.tileLayer.addTo(mymap);
+            
+            </script>
+            </div>
+        </div>
+    </section>
     <section>
         <!-- Bas de page contenu à définir -->
     </section>
